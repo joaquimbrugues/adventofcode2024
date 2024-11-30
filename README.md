51795a8a24a6ea026c7790c4587bf1f74b914b68
Joaquim <devjoaquim@gmail.com>
2024-11-30 17:17:14 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2024
+
+This is my humble attempt to solve all of the puzzles for [Advent of Code 2024](https://adventofcode.com/2024).
+
+I chose [Rust](https://doc.rust-lang.org/stable/book/) because I was looking for a challenge and the language seems to be full of possibilities.
+
+Many thanks to the wonderful community at [r/adventofcode](https://www.reddit.com/r/adventofcode/).
---
+++ template.rs
@@ -0,0 +1,55 @@
+use std::{env,fs,process};
+
+fn run1(input: &str) -> u32 {
+    0
+}
+
+fn run2(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    let mut args = env::args();
+    let filepath;
+    args.next();
+    if let Some(s) = args.next() {
+        filepath = s;
+    }
+    else {
+        eprintln!("Give me a file name! I must feeds on files! Aaargh!");
+        process::exit(1);
+    }
+
+    let input = fs::read_to_string(filepath).unwrap();
+
+    let res = run1(&input);
+    println!("{res}");
+}
+
+#[test]
+fn example1() {
+    let input = fs::read_to_string("test.txt").unwrap();
+    let res = run1(&input);
+    assert_eq!(res,42);
+}
+
+//#[test]
+//fn input1() {
+    //let input = fs::read_to_string("input.txt").unwrap();
+    //let res = run1(&input);
+    //assert_eq!(res,42);
+//}
+
+//#[test]
+//fn example2() {
+    //let input = fs::read_to_string("test.txt").unwrap();
+    //let res = run2(&input);
+    //assert_eq!(res,42);
+//}
+
+//#[test]
+//fn input2() {
+    //let input = fs::read_to_string("input.txt").unwrap();
+    //let res = run2(&input);
+    //assert_eq!(res,42);
+//}
