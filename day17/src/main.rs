2980d634af8c2e28ef030fe8a3fa1d256d65e260
Joaquim <devjoaquim@gmail.com>
2024-12-17 17:45:21 +0100

    Day 17, Part 2

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,12 +1,86 @@
 use std::{env,fs,process};
 
-fn resolve_combo(code: u8, reg: &[u32;3]) -> u32 {
-    match code {
-        0..=3 => code as u32,
-        4 => reg[0],
-        5 => reg[1],
-        6 => reg[2],
-        _ => panic!("Unexpected combo code {code}"),
+
+struct Computer<'a> {
+    regA: u64,
+    regB: u64,
+    regC: u64,
+    instructions: &'a [u8],
+    output: Vec<u8>,
+}
+
+impl<'a> Computer<'a> {
+    fn new(regA: u64, regB: u64, regC: u64, instructions: &'a [u8]) -> Self {
+        Self { regA, regB, regC, instructions, output: vec![], }
+    }
+
+    fn resolve_combo(&self, code: u8) -> u64 {
+        match code {
+            0..=3 => code as u64,
+            4 => self.regA,
+            5 => self.regB,
+            6 => self.regC,
+            _ => panic!("Unexpected combo code {code}"),
+        }
+    }
+
+    fn execute(&mut self) -> &Vec<u8> {
+        // Initialize instruction pointer
+        let mut inptr = 0;
+
+        // Follow the program
+        while inptr < self.instructions.len() {
+            //for _ in 0..20 {
+            let operand = self.instructions[inptr+1];
+            match self.instructions[inptr] {
+                // ADV
+                0 => {
+                    self.regA >>= self.resolve_combo(operand);
+                    inptr += 2;
+                },
+                // BXL
+                1 => {
+                    self.regB ^= operand as u64;
+                    inptr += 2;
+                }
+                // BST
+                2 => {
+                    self.regB = self.resolve_combo(operand) % 8;
+                    inptr += 2;
+                },
+                // JNZ
+                3 => {
+                    if self.regA == 0 {
+                        inptr += 2;
+                    } else {
+                        inptr = operand as usize;
+                    }
+                }
+                // BXC
+                4 => {
+                    self.regB ^= self.regC;
+                    inptr += 2;
+                },
+                // OUT
+                5 => {
+                    let out = self.resolve_combo(operand) % 8;
+                    self.output.push(out as u8);
+                    inptr += 2;
+                },
+                // BDV
+                6 => {
+                    self.regB = self.regA >> self.resolve_combo(operand);
+                    inptr += 2;
+                },
+                //CDV
+                7 => {
+                    self.regC = self.regA >> self.resolve_combo(operand);
+                    inptr += 2;
+                }
+                _ => unreachable!(),
+            }
+        }
+        &self.output
     }
 }
 
@@ -26,84 +100,52 @@ fn run1(input: &str) -> String {
         }
     }
     // Read the program
-    let prog = prog.strip_prefix("Program: ").unwrap().trim();
-    let codes: Vec<u8> = prog.split(',').map(|tok| tok.parse().unwrap()).collect();
-    // Initialize instruction pointer
-    let mut inptr = 0;
-    // Initialize output
-    let mut output = String::new();
-
-    // Follow the program
-    while inptr < codes.len() {
-    //for _ in 0..20 {
-        let operand = codes[inptr+1];
-        match codes[inptr] {
-            // ADV
-            0 => {
-                registry[0] /= 2u32.pow(resolve_combo(operand, &registry));
-                //println!("InPtr: {inptr}, ADV: A = {}", registry[0]);
-                inptr += 2;
-            },
-            // BXL
-            1 => {
-                registry[1] ^= operand as u32;
-                //println!("InPtr: {inptr}, BXL: B = {}", registry[1]);
-                inptr += 2;
-            }
-            // BST
-            2 => {
-                registry[1] = resolve_combo(operand, &registry) % 8;
-                //println!("InPtr: {inptr}, BST: B = {}", registry[1]);
-                inptr += 2;
-            },
-            // JNZ
-            3 => {
-                //print!("InPtr: {inptr}, JNZ: ");
-                if registry[0] == 0 {
-                    inptr += 2;
-                    //print!("A == 0, jump to {inptr}\n");
-                } else {
-                    inptr = operand as usize;
-                    //print!("A != 0, jump to {inptr}\n");
-                }
-            }
-            // BXC
-            4 => {
-                //print!("InPtr: {inptr}, BXC: B = {}, C = {}, ", registry[1], registry[2]);
-                registry[1] ^= registry[2];
-                //print!("res = {}\n", registry[1]);
-                inptr += 2;
-            },
-            // OUT
-            5 => {
-                let out = resolve_combo(operand, &registry) % 8;
-                if output.len() > 0 { output.push(','); }
-                output.push(char::from_digit(out,10).unwrap());
-                //println!("InPtr: {inptr}, OUT: out = {output}");
-                inptr += 2;
-            },
-            // BDV
-            6 => {
-                registry[1] = registry[0] / 2u32.pow(resolve_combo(operand, &registry));
-                //println!("InPtr: {inptr}, BDV: B = {}", registry[1]);
-                inptr += 2;
-            },
-            //CDV
-            7 => {
-                registry[2] = registry[0] / 2u32.pow(resolve_combo(operand, &registry));
-                //println!("InPtr: {inptr}, CDV: C = {}", registry[1]);
-                inptr += 2;
-            }
-            _ => unreachable!(),
-        }
-    }
-
+    let codes: Vec<u8> = prog.strip_prefix("Program: ").unwrap().trim()
+        .split(',')
+        .map(|tok| tok.parse().unwrap())
+        .collect();
+    let mut computer = Computer::new(registry[0], registry[1], registry[2], &codes);
     // Return
-    output
+    computer.execute().into_iter().map(|n| n.to_string()).collect::<Vec<_>>().join(",")
 }
 
-fn run2(input: &str) -> u32 {
-    0
+// FACTORS TO EXPLOIT
+// 1- Each run of the program depends only on the 3 last digits of A (in both tests and our input)
+// 2- There is exactly one jump instruction at the end, checking whether A is zero
+//
+// IDEA:
+// Build possible A's by chunks, by producing candidates cummulatively matching increasing tail
+// ends of the program input
+fn run2(input: &str) -> u64 {
+    let codes: Vec<u8> = input
+        .split_once("\n\n").unwrap().1
+        .strip_prefix("Program: ").unwrap().trim()
+        .split(',')
+        .map(|tok| tok.parse().unwrap())
+        .collect();
+
+    let mut min_a = u64::MAX;
+    let mut candidates = (0..8).collect::<Vec<_>>();
+    for cutoff in (0..codes.len()).rev() {
+        let mut next_candidates = Vec::with_capacity(8 * candidates.len());
+        for &a in &candidates {
+            let mut  computer = Computer::new(a, 0, 0, &codes);
+            let output = computer.execute();
+            if *output == codes[cutoff..] {
+                // This possible A yields all outputs from cutoff
+                // Check if this is finished
+                if cutoff == 0 {
+                    min_a = std::cmp::min(min_a, a);
+                } else {
+                    // We still need to generate more outputs, add more candidates
+                    (0..8).for_each(|k| next_candidates.push((a * 8) + k));
+                }
+            }
+        }
+        candidates = next_candidates;
+    }
+
+    min_a
 }
 
 fn main() {
@@ -120,7 +162,7 @@ fn main() {
 
     let input = fs::read_to_string(filepath).unwrap();
 
-    let res = run1(&input);
+    let res = run2(&input);
     println!("{res}");
 }
 
@@ -140,14 +182,14 @@ fn input1() {
 
 #[test]
 fn example2() {
-    let input = fs::read_to_string("test.txt").unwrap();
+    let input = fs::read_to_string("test2.txt").unwrap();
     let res = run2(&input);
     assert_eq!(res, 117440);
 }
 
-//#[test]
-//fn input2() {
-    //let input = fs::read_to_string("input.txt").unwrap();
-    //let res = run2(&input);
-    //assert_eq!(res,42);
-//}
+#[test]
+fn input2() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let res = run2(&input);
+    assert_eq!(res, 37221270076916);
+}
